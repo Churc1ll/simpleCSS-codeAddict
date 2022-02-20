db53109c8778931352d31a9023c2c7e80ea771cc
Churchill <metallic.gears.gmail.com>
2022-02-20 15:23:38 +0300

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(() => {
-  const menuBtn = document.querySelector('.menu-btn');
-  const btns = Array.from(menuBtn.children);
+  // const btns = Array.from(menuBtn.children);
   const changeNav = () => {
     $('.links').toggle(1500);
     // $('#menu-btn').toggleClass('turn');
@@ -9,3 +8,29 @@ $(document).ready(() => {
   };
   menuBtn.onclick = changeNav;
 });
+const menuBtn = document.querySelector('.menu-btn');
+
+const handleIntersect = (entries) => {
+  console.log(entries);
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+    // entry.target.previousSibling.previousSibling.style.color = 'var(--light)';
+    entry.target.style.width='28rem';
+    entry.target.style.border = '3px solid red';
+    console.log(entry.target.previousSibling.previousSibling);
+    // entry.target.previousSibling.previousSibling.style.color = 'red';
+  } else { entry.target.style.width = '0';
+}
+  })    
+}
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(handleIntersect, options);
+const movers = document.querySelectorAll('.move');
+movers.forEach((move) => {
+  observer.observe(move);
+})
